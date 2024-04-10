2cf76d7e1b15851a6a9fe8e25c77415aa5d7c35d
Preston McCallum <prestonscottmccallum@gmail.com>
2024-04-10 11:28:06 -0400

    pushing somewhat formatted stuff

--- vue/src/components/Castle.vue
+++ vue/src/components/Castle.vue
@@ -1,32 +1,61 @@
 <template>
-<router-link :to="{ name: 'castleDetail', params: { castleId: castle.castleId } }" class="card" style="max-width: 540px;"> <!--adding castleId as param-->
-  <div class="row-no-gutters">
-    <div class="image-container">
-        <img id="bannerPhoto" :src = "castle.castleBannerPhoto" alt = "bannerPhoto">
-    </div>
-      <div class="card-body">
+  <router-link :to="{ name: 'castleDetail', params: { castleId: castle.castleId } }" class="castle-card">
+    <div class="castle-card-body">
+      <img class="castle-image" :src="castle.castleBannerPhoto" alt="bannerPhoto">
+      <div class="castle-content">
         <h5 class="castle-name">{{ castle.castleName }}</h5>
         <p class="castle-desc">{{ castle.shortDesc }}</p>
-        <p class="castle-address"><small class="text-muted">{{ castle.address }}</small></p>
+        <p class="castle-address"><small>{{ castle.address }}</small></p>
       </div>
-  </div>
-</router-link>
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
-    props: ['castle']
-
+  props: ['castle']
 }
 </script>
 
+
+
 <style scoped>
 
-div.image-container img {
-    aspect-ratio: 1/1;
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
+.castle-card {
+  display: block;
+  max-width: 33rem;
+  border: 1.5px solid #000000; 
+  border-radius: 20px; /* Rounded corners */
+  overflow: hidden; /* Ensures the content fits within the border */
+  text-decoration: none; /* Removes underline from link */
+  color: inherit; /* Inherits text color */
+  transition: transform 0.2s; /* Smooth transformation on hover */
+  margin-top: 5rem;
+}
+.castle-card:hover {
+  transform: scale(1.02); /* Slight zoom on hover */
+}
+.castle-card-body {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Aligns items vertically center */
+}
+.castle-image {
+  aspect-ratio: 1/1;
+  width: 100%; /* Fixed width, adjust as needed */
+  height: 100%; /* Fixed height, adjust as needed */
+  object-fit: cover;
+  overflow: hidden; /* Ensures the image covers the area */
+  border-radius: 10px;
+}
+.castle-content {
+  flex: 1; 
+}
+.castle-name, .castle-desc, .castle-address {
+  margin: 7px 0; 
+  text-align: center;
+}
+.castle-address small {
+  color: #6C757D; 
 }
 </style>
