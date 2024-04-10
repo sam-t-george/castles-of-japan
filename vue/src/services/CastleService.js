3ba543444bd600923b7735da6f11d8f6fb7bf365
Samuel <samueltoddgeorge@gmail.com>
2024-04-10 11:09:00 -0400

    castle search - still has submit button

--- vue/src/services/CastleService.js
+++ vue/src/services/CastleService.js
@@ -8,6 +8,25 @@ export default {
     searchForCastles(name) {
         let url = '/castle/search/';
         if (name) {
+            if (name.toLowerCase().includes('castle')) {
+                name = name.replace('castle', '');
+            }
+            if (name.toLowerCase().includes('castl')) {
+                name = name.replace('castl', '');
+            }
+            if (name.toLowerCase().includes('cast')) {
+                name = name.replace('cast', '');
+            }
+            if (name.toLowerCase().includes('cas')) {
+                name = name.replace('cas', '');
+            }
+            if (name.toLowerCase().includes('ca')) {
+                name = name.replace('ca', '');
+            }
+            if (name.toLowerCase().includes('c')) {
+                name = name.replace('c', '');
+            }
+            
             url += name.toLowerCase().replace(/\s/g, '');
         }
         return axios.get(url);
