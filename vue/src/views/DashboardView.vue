cca1ed27d4f2fb42f3c721a797e8d3b9244697ad
Jonathan Nahay <jknahay@gmail.com>
2024-04-11 21:56:49 -0400

    pull

--- vue/src/views/DashboardView.vue
+++ vue/src/views/DashboardView.vue
@@ -1,3 +1,12 @@
 <template>
-    <p>Dashboard</p>
-</template>
+    <div class="container">
+      <div class="calendar-column">
+            CALENDAR
+      </div>
+      <div class="cards-column">
+        <div class="cards">
+          CARDS
+        </div>
+      </div>
+    </div>
+  </template>
