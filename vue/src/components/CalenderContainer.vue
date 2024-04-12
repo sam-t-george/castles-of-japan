ae43381b38233f1167dd35e2744e0fff454cf35b
Preston McCallum <prestonscottmccallum@gmail.com>
2024-04-12 13:06:55 -0400

    pulling

--- vue/src/components/CalenderContainer.vue
+++ vue/src/components/CalenderContainer.vue
@@ -2,16 +2,21 @@
 <script setup>
 import { ref } from "vue";
 
-
 const date = ref();
 
 </script>
 
+
+
 <template>
-    <div class="card flex justify-content-center" >
-         <Calendar v-model="date" inline showWeek class="Calendar">  </Calendar> 
-        
+    <div class="card flex justify-content-center">
+         <Calendar v-model="date" inline showWeek class="Calendar" >  </Calendar> 
+         
     </div>
+
+
+
+
 </template>
 
 
