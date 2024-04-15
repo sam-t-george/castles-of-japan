7d6fc45fffdcf7f1ce59572c82addb115fefb5f2
Andrew <doolittleandrew8@gmail.com>
2024-04-15 15:52:17 -0400

    "updated dashboard'

--- vue/src/views/DashboardView.vue
+++ vue/src/views/DashboardView.vue
@@ -1,65 +1,40 @@
 <template>
-    <div class="main-container">
-        <div class="calendar-container">
-            <CalenderContainer />
+  <div class="main-container">
+    <div class="calendar-container">
+      <CalenderContainer />
+    </div>
+    <div class="event-cards">
+      <h2>Event Cards </h2>
+      <div class="card2 card mb-3" style="max-width: 540px;">
+        <div class="row g-0 align-items-center">
+          <div class="col-sm-4 col-5">
+            <img src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg"
+              class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-sm-8 col-7">
+            <div class="card-body">
+              <h5 class="card-title"></h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+              <p class="card-text d-none d-sm-block"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
         </div>
-        <div class="event-cards">
-            <h2>Event Cards </h2>
-            <div class="card2 card mb-3" style="max-width: 540px;">
-  <div class="row g-0 align-items-center">
-    <div class="col-sm-4 col-5">
-      <img src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-sm-8 col-7">
-      <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text d-none d-sm-block"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
-    </div>
-  </div>
-</div>
 
     </div>
   </div>
 </template>
        
 
-    <div class="event-cards">
-
-      <body id="body">
-        <section id="castleList">
-          <Castle v-for="currentCastle in castles" v-bind:key="currentCastle.castleId" v-bind:castle="currentCastle" />
-        </section>
-      </body>
-
-
-      <!-- Convert vertical bootstrap card to horizontal -->
-      <div class="card1 card align-items-center" style="max-width: 540px;">
-        <img src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-          <a href="#" class="btn btn-primary">Learn More</a>
-        </div>
-      </div>
-
-  
-
-
-    </div>
-
-  </div>
-</template>
 
 <script>
 import Castle from '../components/Castle.vue';
 export default {
-    components: {
-        CalenderContainer
-    },
-    props:['castle']
+  components: {
+    
+  },
+  props: ['castle']
 }
 </script>
 
