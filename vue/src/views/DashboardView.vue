48acbefa9abdc92c2ecf2c1c372db9bd9571ec7a
Andrew <doolittleandrew8@gmail.com>
2024-04-15 15:47:28 -0400

    4/15

--- vue/src/views/DashboardView.vue
+++ vue/src/views/DashboardView.vue
@@ -1,23 +1,46 @@
 <template>
-    <div class="main-container">
-        <div class="calendar-container">
-            <CalenderContainer />
-        </div>
-        <div class="event-cards">
-            event cards 
-        </div>
+  <div class="main-container">
+
+    <div class="card flex justify-content-center">
+      <Calendar v-model="date" inline class="Calendar"> </Calendar>
     </div>
+
+    <div class="event-cards">
+
+      <body id="body">
+        <section id="castleList">
+          <Castle v-for="currentCastle in castles" v-bind:key="currentCastle.castleId" v-bind:castle="currentCastle" />
+        </section>
+      </body>
+
+
+      <!-- Convert vertical bootstrap card to horizontal -->
+      <div class="card1 card align-items-center" style="max-width: 540px;">
+        <img src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="btn btn-primary">Learn More</a>
+        </div>
+      </div>
+
+  
+
+
+    </div>
+
+  </div>
 </template>
 
 <script>
-import CalenderContainer from '../components/CalenderContainer.vue';
-
-
-
+import Castle from '../components/Castle.vue';
 export default {
-    components: {
-        CalenderContainer
-    }
+
+  components: {
+    Castle,
+  },
+  props: ['castle']
 }
 </script>
 
@@ -43,4 +66,24 @@ export default {
   height: 100vh;
 }
 
-</style>
+@media screen and (max-width: 567px) {
+  .card-text {
+    margin-bottom: 0.5em;
+  }
+}
+
+.card {
+  margin: 5%;
+  flex-direction: row;
+}
+
+.card-body {
+  padding: 0.5em 1em;
+}
+
+.card1.card img {
+  max-width: 12em;
+  height: 100%;
+  border-bottom-left-radius: calc(0.25rem - 1px);
+  border-top-left-radius: calc(0.25rem - 1px);
+}</style>
